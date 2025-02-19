78d5f1d502b2be43afc3cedb4d018071e52565c7
vikasthakurr <vikaskumar20012001@gmail.com>
2025-02-19 19:17:42 +0530

    dom

--- Agenda.js
+++ Agenda.js
@@ -1,60 +1,44 @@
 // today's agenda
-// 0. working with objects continue
-// 1. rest parameter in javascript
-// 2. spread operator in javascript
-// 3. shallow and deep copy of object in javascript
-// 4. destructuring  in javascript
-// 5. template literals in javascript
-// 6. IIFE in javascript
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // DOM in javascript
-// 1. document object model in javascript
-// 2. event in javascript
+// document object model in javascript
+// method of selection of element in javascript
+// modifying element
+// creation and deletion of element in javascript
+// selection of attributes
+// event in javascript
+// event listener in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 3. event target in javascript
 // 4. event bubbling in javascript
 // 5. event delegation in javascript
 // 6. event propagation in javascript
 // 7. event capturing in javascript
-// 8. event listener in javascript
-
-
-
 
 // 9. callback function in javascript
 // 10. promise in javascript
 // 11. async and await in javascript
 // Fetch function in js
-
-
-
