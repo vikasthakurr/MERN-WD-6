86ccc96b3f693ef97d19d8cbb57eb0afadf69d65
vikasthakurr <vikaskumar20012001@gmail.com>
2025-02-20 18:50:39 +0530

    TODO using DOM

--- Agenda.js
+++ Agenda.js
@@ -1,12 +1,10 @@
 // today's agenda
-// DOM in javascript
-// document object model in javascript
-// method of selection of element in javascript
-// modifying element
-// creation and deletion of element in javascript
-// selection of attributes
-// event in javascript
 // event listener in javascript
+// Famous Events in DOM
+// to-do List in javascript using DOM
+// event bubbling in javascript
+// event capturing in javascript
+// event delegation in javascript
 
 
 
@@ -32,11 +30,23 @@
 
 
 
-// 3. event target in javascript
-// 4. event bubbling in javascript
-// 5. event delegation in javascript
-// 6. event propagation in javascript
-// 7. event capturing in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 9. callback function in javascript
 // 10. promise in javascript
