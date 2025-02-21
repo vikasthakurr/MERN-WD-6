2d02b806ce8c178672045c2d23c5a7188bbd4d9b
vikasthakurr <vikaskumar20012001@gmail.com>
2025-02-21 13:15:27 +0530

    updated

--- Agenda.js
+++ Agenda.js
@@ -1,10 +1,9 @@
 // today's agenda
-// event listener in javascript
-// Famous Events in DOM
-// to-do List in javascript using DOM
 // event bubbling in javascript
 // event capturing in javascript
 // event delegation in javascript
+// callback function in javascript
+// callback hell  or pyramid of doom in js
 
 
 
@@ -43,12 +42,6 @@
 
 
 
-
-
-
-
-
-// 9. callback function in javascript
-// 10. promise in javascript
-// 11. async and await in javascript
+// promise in javascript
+// async and await in javascript
 // Fetch function in js
