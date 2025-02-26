e4258d2856dcb61f2e3aa7a5ab6507bcfb7c4f6a
vikasthakurr <vikaskumar20012001@gmail.com>
2025-02-27 00:29:27 +0530

    renaming done

--- Agenda.js
+++ Agenda.js
@@ -1,25 +1,4 @@
 // today's agenda
-// event bubbling in javascript
-// event capturing in javascript
-// event delegation in javascript
-// callback function in javascript
-// callback hell  or pyramid of doom in js
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // promise in javascript
 // async and await in javascript
 // Fetch function in js
